d86138c156b4f78613b396a6a41ee018fe3b4d9b
Joe Karlsson <josephcarlson1@gmail.com>
2017-03-11 13:59:33 -1000

    got the map populated with data from m2x

---
+++ public/js/map.js
@@ -0,0 +1,42 @@
+window.onload = function (){
+  getCheckPoints();
+}
+
+function getCheckPoints() {
+  var req = new XMLHttpRequest();
+  req.addEventListener('load', receiveData);
+  req.open('GET', `/api/checkpoint`)
+  req.send();
+}
+
+function receiveData(){
+  let data = JSON.parse(this.responseText).json.values;
+  addPointsToMap(data)
+}
+
+function addPointsToMap(data) {
+  // let parsedData = JSON.parse(data[0].values['check-points']);
+  data.map((point, i) => {
+    let parsedData = JSON.parse(point.values['check-points']);
+    let lat = parsedData.lat;
+    let lon = parsedData.lon;
+    // Add points from M2X on the map
+    if (lat || lon) {
+      var marker = L.marker([lat, lon]).addTo(mymap);
+      marker.bindPopup(`<b>Timestamp</b><br>${parsedData.timestamp}.`);
+    }
+  });
+}
+
+var mymap = L.map('mapid').setView([21.2858534, -157.8064349], 15);
+
+L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+
+// https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'mapbox.streets'
+}).addTo(mymap);
+
