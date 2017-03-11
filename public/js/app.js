3931dea9faab06137cf1832dd94b320220a7201c
Sean Nakamura <seanenakamura@gmail.com>
2017-03-11 12:11:09 -1000

    repurposes app.js for landing page

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,3 @@
-console.log('check')
-
-// endpoint /api/checkpoint/lat/:param/long/:param
-window.onload = function(){
-  var geo = navigator.geolocation;
-  geo.getCurrentPosition(function (position) {
-    sendLocation(position.coords.latitude, position.coords.longitude)
-  })
-
-  function sendLocation(lat, lng) {
-    var req = new XMLHttpRequest();
-    req.addEventListener('load', receiveRotation);
-    req.open('POST', `/api/checkpoint/lat/${lat}/long/${lng}`)
-    req.send();
-  }
-
-  function receiveRotation(){
-    console.log(this.responseText)
-  }
-
+window.onload = function (){
+  console.log('Welcome to GAPTCHA!')
 }
