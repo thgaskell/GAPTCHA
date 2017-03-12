8d0d4f0dfc8567f7e5bccd1e913768fcef15d266
Joe Karlsson <josephcarlson1@gmail.com>
2017-03-11 17:06:17 -1000

    win condition shows on homepage now

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 // endpoint /api/checkpoint/capture/lat/:param/long/:param
 window.onload = function (){
-  console.log('Welcome to GAPTCHA!')
   var geo = navigator.geolocation;
 
   var winBtn = document.getElementById('win-button');
@@ -8,12 +7,18 @@ window.onload = function (){
     geo.getCurrentPosition(function (position) {
       winnahwinnah(position.coords.latitude, position.coords.longitude)
     })
-  })
+  });
 
   function winnahwinnah(lat, lng){
     var req = new XMLHttpRequest();
     req.open('GET', `/api/checkpoint/capture/lat/${lat}/long/${lng}`)
+    req.addEventListener('load', receiveWinData);
     req.send();
+  };
 
-  }
-}
+  function receiveWinData(){
+    let data = JSON.parse(this.responseText);
+    var winButton = document.getElementById('win-status');
+    winButton.innerHTML = data.message;
+  };
+};
