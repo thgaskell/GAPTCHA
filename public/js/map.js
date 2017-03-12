ed78e043d54502f6d6307753fc67be10975876a0
Joe Karlsson <josephcarlson1@gmail.com>
2017-03-11 17:14:53 -1000

    fixed map issue

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,27 @@
 window.onload = function (){
   getCheckPoints();
+
+  var geo = navigator.geolocation;
+
+  var winBtn = document.getElementById('win-button');
+  winBtn.addEventListener('click', function (evt){
+    geo.getCurrentPosition(function (position) {
+      winnahwinnah(position.coords.latitude, position.coords.longitude)
+    })
+  });
+
+  function winnahwinnah(lat, lng){
+    var req = new XMLHttpRequest();
+    req.open('GET', `/api/checkpoint/capture/lat/${lat}/long/${lng}`)
+    req.addEventListener('load', receiveWinData);
+    req.send();
+  };
+
+  function receiveWinData(){
+    let data = JSON.parse(this.responseText);
+    var winButton = document.getElementById('win-status');
+    winButton.innerHTML = data.message;
+  };
 }
 
 function getCheckPoints() {
